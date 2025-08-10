dc8e229fe4aafc9628e1f783d00b4b944e0b8d9b
Jesper-LiveFlow - PC <122182765+Jesper-LiveFlow@users.noreply.github.com>
2025-08-10 23:13:12 +0200

    Setup Spring Boot Backend

---
+++ apps/backend/src/main/java/com/jesperweijnen/trivia/TriviaApplication.java
@@ -0,0 +1,15 @@
+package com.jesperweijnen.trivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaApplication.class, args);
+
+        System.out.println("TriviaApplication started.");
+    }
+
+}
