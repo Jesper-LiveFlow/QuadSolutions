4e4965b4df830caee19e8a4ffb3caa21e665bf97
Jesper-LiveFlow - PC <122182765+Jesper-LiveFlow@users.noreply.github.com>
2025-08-11 22:13:24 +0200

    Added Question DTO
    This Data Transfer Object defines the json object that is send over my trivia API to the user.

---
+++ apps/backend/src/main/java/com/jesperweijnen/trivia/dto/QuestionDto.java
@@ -0,0 +1,15 @@
+package com.jesperweijnen.trivia.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class QuestionDto {
+    private final String type;
+    private final String difficulty;
+    private final String category;
+    private final String question;
+    private final List<String> answers;
+}
