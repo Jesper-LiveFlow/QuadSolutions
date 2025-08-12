1118ce8bfe33e09c31d9369a85ff4202de555595
jesperweijnen <jesper@liveflow.nl>
2025-08-12 11:10:51 +0200

    Frontend: Implemented some test logic on home view

--- apps/frontend/src/views/QS_ViewHome.vue
+++ apps/frontend/src/views/QS_ViewHome.vue
@@ -2,4 +2,19 @@
   <div>View Home</div>
 </template>
 
-<script setup></script>
+<script setup>
+/** VUE IMPORTS */
+import { onMounted } from "vue";
+/** COMPOSABLE IMPORTS */
+import { useUuid } from "@/composables/useUserId";
+const { userUuid } = useUuid();
+/** SERVICE IMPORTS */
+import { getQuestions } from "@/api/services";
+
+onMounted(async () => {
+  // const questions = await getQuestions();
+  // console.log(questions);
+
+  console.log("You're UUID: " + userUuid.value);
+});
+</script>
