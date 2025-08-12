1ee4534a93b9836cff61f3d582a11ef8be26cd94
jesperweijnen <jesper@liveflow.nl>
2025-08-12 09:42:24 +0200

    Frontend: Create API endpoints file

---
+++ apps/frontend/src/api/apiEndpoints.ts
@@ -0,0 +1,12 @@
+/**
+ * apiEndpoints.ts
+ * ---------------------------------------
+ * Central configuration for API endpoints
+ *
+ */
+
+/** GET ENDPOINTS */
+export const QUESTIONS: string = "questions";
+
+/** POST ENDPOINTS */
+export const CHECK_ANSWERS: string = "checkanswers";
