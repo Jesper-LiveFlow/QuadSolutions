0533c99ec55860f5b8175c8de949857bf7a7ac2f
jesperweijnen <jesper@liveflow.nl>
2025-08-12 09:42:09 +0200

    Create API config file

---
+++ apps/frontend/src/api/apiConfig.ts
@@ -0,0 +1,20 @@
+/**
+ * apiConfig.ts
+ * --------------------------------
+ * Central API configuration file
+ *
+ */
+
+// BASE_URL: API url of backend
+const BASE_URL: string = "localhost:8080/api";
+// API_TIMEOUT: API timeout in seconds
+const API_TIMEOUT: number = 10000;
+// DEFAULT_HEADERS: Default request headers to tell server what kind of data is sent
+const DEFAULT_HEADERS: string = "application/json";
+
+// Export API_CONFIG object
+export const API_CONFIG = {
+  API_URL: BASE_URL,
+  API_TIMEOUT,
+  DEFAULT_HEADERS,
+};
