649516a70e32e6b02205e08c473a48462a04818f
jesperweijnen <jesper@liveflow.nl>
2025-08-12 13:14:30 +0200

    Frontend: Created colorfull moving background

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <!-- Show Router View -->
-  <router-view />
+  <main
+    class="min-h-screen bg-gradient-to-tr from-indigo-400 via-pink-400 to-yellow-300 flex flex-col justify-between animate-slow-bg-move"
+  >
+    <router-view />
+  </main>
 </template>
 
 <script setup></script>
