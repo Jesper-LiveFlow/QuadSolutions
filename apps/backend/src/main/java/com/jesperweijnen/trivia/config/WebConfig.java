f2e14a93ca16d4138fe666ba4056294365dc24d4
Jesper-LiveFlow - PC <122182765+Jesper-LiveFlow@users.noreply.github.com>
2025-08-13 14:04:21 +0200

    Backend: Changed common folder to "config" folder, makes more sense

--- apps/backend/src/main/java/com/jesperweijnen/trivia/config/WebConfig.java
+++ apps/backend/src/main/java/com/jesperweijnen/trivia/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.jesperweijnen.trivia.common;
+package com.jesperweijnen.trivia.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
