bcb68e41fdb8d7170f43deae62f5350153a20f2b
Jesper-LiveFlow - PC <122182765+Jesper-LiveFlow@users.noreply.github.com>
2025-08-13 14:24:32 +0200

    Backend: Removed logging statement in main function

--- apps/backend/src/main/java/com/jesperweijnen/trivia/TriviaApplication.java
+++ apps/backend/src/main/java/com/jesperweijnen/trivia/TriviaApplication.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TriviaApplication {
     public static void main(String[] args) {
         SpringApplication.run(TriviaApplication.class, args);
-
-        System.out.println("TriviaApplication started.");
     }
 }
